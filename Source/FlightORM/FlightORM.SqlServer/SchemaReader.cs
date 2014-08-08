054b2d061875efe18243474e8ca32155b16fe6a3
github@404htm.com <github@404htm.com>
2014-08-07 22:59:01 -0400

    Project Setup

---
+++ Source/FlightORM/FlightORM.SqlServer/SchemaReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.SqlServer
+{
+    public class SchemaReader
+    {
+        private SchemaReader()
+        {
+
+        }
+
+        public SchemaReader Read(string connectionString)
+        {
+
+        }
+
+
+    }
+}
