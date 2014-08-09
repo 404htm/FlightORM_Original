86af673b1c2017e69cdba03e6e6178c6a312ea26
github@404htm.com <github@404htm.com>
2014-08-09 15:27:11 -0400

    Adding TestDB Project

--- Source/FlightORM/FlightORM.CommonTests/TypeMapTests.cs
+++ Source/FlightORM/FlightORM.CommonTests/TypeMapTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FlightORM.Common;
 using System.Linq;
+using System.IO;
 
 namespace FlightORM.CommonTests
 {
@@ -9,7 +10,7 @@ namespace FlightORM.CommonTests
 	public class TypeMapTests
 	{
 		[TestMethod]
-		public void LoadFile()
+		public void LoadEmbeddedFileV1()
 		{
 			var str = Helpers.GetInputFile("sqlserver_typemap1.json");
 			var map = TypeMap.Load(str);
@@ -17,6 +18,23 @@ namespace FlightORM.CommonTests
 			Assert.IsNotNull(map);
 			Assert.IsTrue(map.Entries.Any());
 			Assert.IsNotNull(map.Source);
+		}
+
+		[TestMethod]
+		public void TestSerializationAndDeserialization()
+		{
+			var map = new TypeMap();
+			map.Entries.Add(new TypeInfo());
+			map.Entries.Add(new TypeInfo());
+			map.Source = "Test Source";
+
+			var f = Path.GetTempFileName();
+			map.Save(f);
+			var map2 = TypeMap.Load(f);
+
+			Assert.IsNotNull(map2);
+			Assert.IsTrue(map.Source == map2.Source);
+			Assert.IsTrue(map.Entries.Count == map2.Entries.Count());
 		}
 	}
 }
