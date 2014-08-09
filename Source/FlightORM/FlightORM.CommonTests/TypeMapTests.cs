69680a59884ce02e35ba810f2b2d32e855c2b0ff
github@404htm.com <github@404htm.com>
2014-08-09 14:10:29 -0400

    Fixed typemap encodings, setup testproject

---
+++ Source/FlightORM/FlightORM.CommonTests/TypeMapTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlightORM.Common;
+using System.Linq;
+
+namespace FlightORM.CommonTests
+{
+	[TestClass]
+	public class TypeMapTests
+	{
+		[TestMethod]
+		public void LoadFile()
+		{
+			var str = Helpers.GetInputFile("sqlserver_typemap1.json");
+			var map = TypeMap.Load(str);
+
+			Assert.IsNotNull(map);
+			Assert.IsTrue(map.Entries.Any());
+			Assert.IsNotNull(map.Source);
+		}
+	}
+}
