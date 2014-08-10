9beda29fc8337bedc1a67478beac8e05485c1365
github@404htm.com <github@404htm.com>
2014-08-09 22:27:45 -0400

    Added files missing from last commit

---
+++ Source/FlightORM/FlightORM.Common/TypeMap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.Common
+{
+	[DataContract]
+	public class TypeMap : ConfigBase<TypeMap>
+	{
+		public TypeMap()
+		{
+			Entries = new List<TypeInfo>();
+		}
+
+		[DataMember]
+		public List<TypeInfo> Entries { get; set; }
+		[DataMember]
+		public String Source { get; set;}
+	}
+
+
+	[DataContract]
+	public class TypeInfo
+	{
+	    [DataMember]
+        public string CodeType { get; set; }
+        [DataMember]
+        public bool IsValueType { get; set; }
+        [DataMember]
+        public string DbType { get; set; }
+        [DataMember]
+        public string Accessor { get; set; }
+		[DataMember]
+        public string Enumeration { get; set; }
+    }
+}
---
+++ Source/FlightORM/FlightORM.SqlServer/StoredProcAnalysis.cs
@@ -0,0 +1,53 @@
+﻿using FlightORM.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.SqlServer
+{
+	public class StoredProcAnalysis
+	{
+		string _connectionString;
+
+		public StoredProcAnalysis(string connectionString)
+		{
+			 _connectionString = connectionString;
+		}
+
+		public IEnumerable<StoredProcedure> GetProcedures()
+		{
+			using (var cnn = new SqlConnection(_connectionString))
+			{
+				cnn.Open();
+				var cmd = new SqlCommand("select routine_schema, specific_name, specific_catalog, created, last_altered from information_schema.routines where routine_type = 'PROCEDURE'", cnn);
+				cmd.CommandType = CommandType.Text;
+				var reader = cmd.ExecuteReader();
+
+				var schema_index = reader.GetOrdinal("routine_schema");
+				var name_index = reader.GetOrdinal("specific_name");
+				var catalog_index = reader.GetOrdinal("specific_catalog");
+				var created_index = reader.GetOrdinal("created");
+				var modified_index = reader.GetOrdinal("created");
+
+				foreach (var r in reader)
+				{
+					var sp = new StoredProcedure
+					{
+						Name = reader.GetString(name_index),
+						Schema = reader.GetString(schema_index),
+						DateCreated = reader.GetDateTime(created_index),
+						DateModified = reader.GetDateTime(modified_index),
+						Catalog = reader.GetString(catalog_index)
+					};
+					yield return sp;
+				}
+			}
+		}
+
+		//public IEnumerable<StoredProcedureDetail> GetProcedure
+	}
+}
