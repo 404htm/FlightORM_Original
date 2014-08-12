6ed5f2cb6c398bffb572ae4aa47c20ca3fc9d059
github@404htm.com <github@404htm.com>
2014-08-11 21:56:56 -0400

    Some sample templates - Incomplete (#4)

---
+++ Source/FlightORM/FlightORM.Core/Templates/StoredProcedure/InputDo.cs
@@ -0,0 +1,31 @@
+﻿//## FILENAME: [ClassName]InputDo.cs
+//## LOCATION: ...
+//## AppliesTo: Classes in SPOutputClasses
+//## GROUPSET: TYPESET, USESET
+
+//## TYPESET.Lazy = ALL WHERE USERFLAGS.CONTAINS('Lazy')
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlightORM.Core.Templates.StoredProcedure
+{
+	public class ClassNameInputDo
+	{
+		//# FOREACH PROP(ALL)
+		Type _propertyName;
+		//# END FOREACH
+
+		//# FOREACH PROP(TYPESET.Other)
+		public Type PropertyName
+		{
+			get { return _propertyName; }
+			set { _propertyName = value; }
+		}
+		//# END FOREACH
+	}
+}
