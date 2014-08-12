12c6438b4a29bef1ad18d21788a4e0f7dc292e7c
github@404htm.com <github@404htm.com>
2014-08-12 19:22:59 -0400

    Building Configuration Classes (#8)

---
+++ Source/FlightORM/FlightORM.Core/Config/StoredProcMapping.cs
@@ -0,0 +1,45 @@
+﻿using FlightORM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.Core.Config
+{
+	[DataContract]
+	public class StoredProcMapping : ConfigBase<StoredProcMapping>
+	{
+		public StoredProcMapping(StoredProcedure definition)
+		{
+			Definiton = definition;
+			FriendlyName = definition.Name.Replace("sp_", "");
+		}
+
+		[DataMember]
+		public int ObjectID { get { return Definiton.Id; }}
+
+		[DataMember]
+		public string FriendlyName { get; set;}
+
+		[DataMember]
+		public string OutputType { get; set; }
+
+		[DataMember]
+		public string AssociatedType { get; set; }
+
+
+		[DataMember]
+		public string InputType { get; set; }
+
+		[DataMember]
+		public StoredProcedure Definiton { get; set; }
+
+		[DataMember]
+		public List<string> SystemFlags { get; set;}
+
+		[DataMember]
+		public List<string> UserFlags { get; set; }
+	}
+}
---
+++ Source/FlightORM/FlightORM.Core/Config/StoredProcSet.cs
@@ -0,0 +1,30 @@
+﻿using FlightORM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.Core.Config
+{
+	[DataContract]
+	public class StoredProcSet : ConfigBase<StoredProcSet>
+	{
+		[DataMember]
+		public string Name { get; set;}
+
+		[DataMember]
+		public List<StoredProcMapping> Procedures { get; set; }
+
+		[DataMember]
+		public string ConnectionName { get; set;}
+
+		internal List<string> GetOutputClassDefinitions()
+		{
+			return null;
+		}
+	}
+
+	 
+}
