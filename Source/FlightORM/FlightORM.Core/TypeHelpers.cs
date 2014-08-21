98d2ff245c8245d54309369b1b459ccf06165eaa
github@404htm.com <github@404htm.com>
2014-08-20 22:25:25 -0400

    Rough type conversion helper to assist with filling params from sample data #26

---
+++ Source/FlightORM/FlightORM.Core/TypeHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightORM.Core
+{
+	public static class TypeHelpers
+	{
+		public static object ConvertToType(string value, string type)
+		{
+			switch (type)
+			{
+				case "Byte[]": return Convert.FromBase64String(value.Trim());
+				//TODO: Figure out other primary types not part of typecode
+			}
+
+			var code = (TypeCode)Enum.Parse(typeof(TypeCode), type);
+			return Convert.ChangeType(value, code);
+
+		}
+	}
+}
