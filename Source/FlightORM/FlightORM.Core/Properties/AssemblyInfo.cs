0078fc1917ace1df279969e642c1e411c36f9a83
github@404htm.com <github@404htm.com>
2014-08-21 21:45:56 -0400

    Added Assembly Info

--- Source/FlightORM/FlightORM.Core/Properties/AssemblyInfo.cs
+++ Source/FlightORM/FlightORM.Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlightORM.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©Kelly Gendron  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Source/FlightORM/FlightORM/Properties/AssemblyInfo.cs
+++ Source/FlightORM/FlightORM/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlightORM")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©Kelly Gendron  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
