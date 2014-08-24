682db5f7ebef64df6d34d9ff635e489113fac431
github@404htm.com <github@404htm.com>
2014-08-23 23:46:39 -0400

    #27: File organization/SPResult cleanup

--- Source/FlightORM/FlightORM.Common/StoredProcedure/SPResult.cs
+++ Source/FlightORM/FlightORM.Common/StoredProcedure/SPResult.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightORM.Common
 {
+	[DataContract]
 	public class SPResult
 	{
-		public SPResult()
+		public SPResult(int index)
 		{
 			Columns = new List<ResultElement>();
+			ResultIndex = index;
+			DateRetrieved = DateTime.UtcNow;
 		}	
-		//todo: source
-		public int ResultIndex { get; set; }
-		public List<ResultElement> Columns { get; private set;}
+		
+		[DataMember] public int ResultIndex { get; private set; }
+		[DataMember] public List<ResultElement> Columns { get; private set;}
+		[DataMember] public DateTime DateRetrieved { get; private set;}
+
 		public bool IsScalar { get {  return Columns.Count() == 1; } }
 		public bool IsAction { get {  return Columns.Count() == 0; } }
 	}
